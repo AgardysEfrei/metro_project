01cfd1a513358ef8243d7d1819f1a0520500704b
Vicen <seixas.vicente78@gmail.com>
2024-10-15 09:59:33 +0200

    Finsalisation du parsing, on renvoie une matrice d'adjacence

--- Class.py
+++ Class.py
@@ -1,5 +1,9 @@
 class Stations:
     def __init__(self,vertex,lines,is_terminal,fork):
+        #vertex list
+        #lines int
+        #is_terminal boolean
+        #fork int
         self.vertex=vertex
         self.nbr_line=lines
         self.is_terminal=is_terminal
