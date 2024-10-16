42c2a249648ec78430dd3774111bdf397a3b83b8
Vicen <seixas.vicente78@gmail.com>
2024-10-16 16:57:13 +0200

    ré-écriture de la classe

--- Class.py
+++ Class.py
@@ -1,12 +1,12 @@
 class Stations:
     def __init__(self,identifier,lines,vertex,is_terminal,fork):
-        #identifier int
+        #identifier str
         #lines list int
         #vertex list tuple int syntax: (line, vertex_identifier)
         #is_terminal list tuple boolean syntax: (line, is_terminal)
         #fork list tuple int syntax : (line, fork)
-        self.lines = lines
+        self.lines = [lines]
         self.identifier = identifier
-        self.vertex=vertex
-        self.is_terminal=is_terminal
-        self.fork=fork
+        self.vertex=[(lines,vertex)]
+        self.is_terminal=[(lines,is_terminal)]
+        self.fork=[(lines,fork)]
